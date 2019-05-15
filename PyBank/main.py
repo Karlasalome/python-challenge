936a305d78d7a8280520be807e52f7688cfbc1e6
Karla <46768393+Karlasalome@users.noreply.github.com>
2019-05-15 19:33:10 -0400

    this was the last one

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,89 @@
-#import csv file and The total number of months included in the dataset
+#    Election Results
+#-------------------------
+#Total Votes: 3521001
+#-------------------------
+#Khan: 63.000% (2218231)
+#Correy: 20.000% (704200)
+#Li: 14.000% (492940)
+#O'Tooley: 3.000% (105630)
+#-------------------------
+
+#import csv file
 import csv 
-input_File ="C:\\Users\\Karla Salome\\Desktop\\COLNYC201904DATA3\\homework\\03-Python\\Instructions\\PyBank\\Resources\\budget_data.csv"
-totalMonths = 0
-totalRev = 0
-pastRev = 0
-highestIncRev = 0
-lowestDecRev = 0
-#create lists to store revenue change. [] saves it as a list.
-revChange=[]
+inputfile ="C:\\Users\\Karla Salome\\Desktop\\COLNYC201904DATA3\\homework\\03-Python\\Instructions\\PyPoll\\Resources\\election_data.csv"
+#create an output text file
+outputfile = "C:\\Users\\Karla Salome\\Desktop\\COLNYC201904DATA3\\homework\\03-Python\\Instructions\\PyPoll\\Resources\\election_output_data.txt"
+# Create list for the csv file
+polls = []
+#create two dictionaries, candidate names and total number of voters per candidate
+dict_polls = {}
+dict_summary = {}
+# Open file and assign to csvfile a name
+with open(inputfile, newline='') as csvfile:
+   # read
+   # split the data on commas
+   # assign to pollreader string variable
+   # all in the same loop
+   pollreader = csv.reader(csvfile, delimiter=',')
+   # Skip header row one time
+   next(pollreader)
+   text_file = open(outputfile, "w")
+   # print to text file
+   text_file.write("Election Results")
+   # Convert pollreader string to list
+   for line in pollreader:
+       polls.append(line)
+   # print to text file
+   text_file.write("\nTotal Votes: " + str(len(polls)))
+   # print to to terminal
+   print("Total Votes: " + str(len(polls)))
+   # Output to text file
+   text_file.write("\n-------------------------")
+   # print to Terminal
+   print("-------------------------")
+   # Convert polls list into dictionary for counting and grouping candidate names 
+   for line in polls:
+       name_key = line[2]
+       if name_key not in dict_polls:
+           # insert name_key into dictionary Summary and initialize to 0
+           dict_polls[name_key] = 0
+       # count the name key inside dictionary
+       dict_polls[name_key] += 1
+# Print to Terminal
+   print("\nKarla Salome Pratts\n \n-ELECTION RESULTS-")
+   # print to text file
+   text_file.write("\n-------------------------")
+   # Print to Terminal
+   print("-------------------------")
+   # Compute the percentages of each name key of dict_polls and insert into dict_summary with 'len' to return number of items inside polls
+   total_polls = len(polls)
+   for name in dict_polls:
+       dict_summary[name] = round((dict_polls[name] / total_polls) * 100)
+       # print to text file
+       text_file.write("\n" + str(name) + ": " + str(dict_summary[name]) + "% " + "(" + str(dict_polls[name]) + ")")
+       # Print to Terminal
+       print(str(name) + ": " + str(dict_summary[name]) + "% " + "(" + str(dict_polls[name]) + ")")
 
-with open(input_File, newline='') as csvfile:
-    #read file
-    #split the data on commas
-    budget_csvreader = csv.reader(csvfile, delimiter=',')
-    #skip header row one time
-    next(budget_csvreader)
-    for row in budget_csvreader:
-        #count total months in csv file
-        totalMonths = totalMonths + 1
-        #count total revenue in csv file using float in case there are decimal points
-        totalRev = totalRev + (float(row[1]))
-        #create a variable that will count the revenue change also with float
-        monthlyRevChange = float(row[1]) - pastRev
-        pastRev = float(row[1])
-        #calculate the average change in revenue
-        avgRevChange = totalRev/totalMonths
-        #find the greatest increase in revenue
-        if monthlyRevChange > highestIncRev:
-            highestIncMonth = row[0]
-            highestIncRev = monthlyRevChange 
-        #find the greatest decrease in revenue
-        if monthlyRevChange < lowestDecRev:
-            lowestDecMonth = row[0]
-            lowestDecRev = monthlyRevChange
+   # highest value
+   highest = 0
+   # Find larget value of the key/value pair inside dictionary and place the key name inside winner
+   for name in dict_summary:
+       if highest < dict_summary[name]:
+           highest = dict_summary[name]
+           winner = name
 
-#print everything, add '$' to currency
+   # print to text file
+   text_file.write("\n-------------------------")
+   # Print to Terminal
+   print("-------------------------")
+   # print to text file
+   text_file.write("\nWinner: " + winner)
+   # Print to Terminal
+   print("Winner: " + winner)
+   # print to text file
+   text_file.write("\n-------------------------")
+   # Print to Terminal
+   print("-------------------------")
 
-print("Karla Salome Pratts -FINANCIAL ANALYSIS-")
-print("_____________________________________")
-print(f"Total Months: {totalMonths}")
-print(f"Total Revenue: ${totalRev}")
-print(f"Average Revenue Change: ${avgRevChange}")
-print(f"Greatest Increase in Revenue: {highestIncMonth} ${highestIncRev}")
-print(f"Greatest Decrease in Revenue: {lowestDecMonth} ${lowestDecRev}")
-
-
+# Close text file
+text_file.close()
